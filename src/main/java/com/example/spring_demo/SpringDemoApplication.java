50c4e8c52913e42c347190dbae80dbc1c37978c8
wangjianweigit <1143036381@qq.com>
2018-10-25 22:59:55 +0800

    commit

--- src/main/java/com/example/spring_demo/SpringDemoApplication.java
+++ src/main/java/com/example/spring_demo/SpringDemoApplication.java
@@ -3,8 +3,6 @@ package com.example.spring_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-
 @SpringBootApplication
 public class SpringDemoApplication {
 
